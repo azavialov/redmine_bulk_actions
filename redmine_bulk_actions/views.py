ef9d576cfcb31926fce22554a48a47370c17fe89
andrey.zavialov@yandex.ru <az@az-server.(none)>
2012-08-17 02:06:36 +0400

    dobavil podderjku activeResource

--- redmine_bulk_actions/views.py
+++ redmine_bulk_actions/views.py
@@ -1,10 +1,17 @@
 # Create your views here.
 from django.template import Context, loader, RequestContext
 from django.http import HttpResponse
+from pyactiveresource.activeresource import ActiveResource
 
+class Project(ActiveResource):
+	_site = 'http://54.245.23.219:8080/'
+	_user = 'admin'
+	_password = 'admin'
 
 def index(request):
-    t = loader.get_template('index/index.html')
+	projects = Project.find()
+	projectslist = projects.attributes
+	t = loader.get_template('index/index.html')
     c = RequestContext(request)
     # c = Context.({
     #    'response' : 'index_action',
